f8c9b1d56d64aae30d9d5bb96dd818289d6e079b
Akhilesh verma <Akhileshverma207@gmail.com>
2026-03-13 23:35:27 +0530

    fix css

--- src/pages/Editor_pdf/Viewer_Editor.jsx
+++ src/pages/Editor_pdf/Viewer_Editor.jsx
@@ -12,26 +12,26 @@ export default function Dashboard() {
       short: "SBI",
       logo: "https://upload.wikimedia.org/wikipedia/commons/c/cc/SBI-logo.svg"
     },
-    {
-      name: "Bank of India",
-      short: "BOI",
-      logo: "https://1000logos.net/wp-content/uploads/2021/06/Bank-of-India-logo.png"
-    },
-    {
-      name: "HDFC Bank",
-      short: "HDFC",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/2/28/HDFC_Bank_Logo.svg"
-    },
-    {
-      name: "ICICI Bank",
-      short: "ICICI",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/1/12/ICICI_Bank_Logo.svg"
-    }
+    // {
+    //   name: "Bank of India",
+    //   short: "BOI",
+    //   logo: "https://1000logos.net/wp-content/uploads/2021/06/Bank-of-India-logo.png"
+    // },
+    // {
+    //   name: "HDFC Bank",
+    //   short: "HDFC",
+    //   logo: "https://upload.wikimedia.org/wikipedia/commons/2/28/HDFC_Bank_Logo.svg"
+    // },
+    // {
+    //   name: "ICICI Bank",
+    //   short: "ICICI",
+    //   logo: "https://upload.wikimedia.org/wikipedia/commons/1/12/ICICI_Bank_Logo.svg"
+    // }
   ];
 
   return (
     <div className="services-page">
-      <Navbar />
+    <Navbar />
 
       <h1>Select Your Bank</h1>
 
